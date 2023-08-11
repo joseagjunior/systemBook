cb91d7c062ef9da75f04a14e599c81230e7b8fff
joseagjunior <dev.joseagjunior@gmail.com>
2023-08-11 11:05:21 -0300

    include scss

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,12 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('assets/css/app.css')}}">
+    @vite(['resources/scss/app.scss'])
     <title>SystemBook</title>
 </head>
 <body>
     <div class="sidebar">
         <a href="">Books</a>
+        <a href="">About</a>
         <a href="">Register</a>
         <a href="">Login</a>
     </div>
